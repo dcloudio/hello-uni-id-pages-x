822040d173efc060982bbab6f3a17c87cfecd2b8
Anne_LXM <54163582+anne-lxm@users.noreply.github.com>
2024-01-30 21:44:16 +0800

    test index.test.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
 const path = require('path');
 module.exports = {
-	testTimeout: 30000,
+	testTimeout: 60000,
 	reporters: [
 		'default'
 	],
 	watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
 	moduleFileExtensions: ['js', 'json'],
 	rootDir: __dirname,
-	// testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)","<rootDir>/uni_modules/uni-id-pages-x/pages/**/*test.[jt]s?(x)"],
-  testMatch: ["<rootDir>/pages/index/index.test.js"],
-  // testMatch: ["<rootDir>/uni_modules/uni-id-pages-x/pages/login/login.test.js"],
+	testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)","<rootDir>/uni_modules/uni-id-pages-x/pages/**/*test.[jt]s?(x)"],
 	testPathIgnorePatterns: ['/node_modules/'],
 	testSequencer: path.join(__dirname, "testSequencer.js")
 }
