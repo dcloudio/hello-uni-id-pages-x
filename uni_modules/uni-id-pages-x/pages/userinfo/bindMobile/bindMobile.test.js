985a8c2bea5c9666650c587e2384f2f145a8c795
anne-xml <liuxiaomei@dcloud.io>
2024-12-26 21:06:47 +0800

    test:update bindMobile.test.js

--- uni_modules/uni-id-pages-x/pages/userinfo/bindMobile/bindMobile.test.js
+++ uni_modules/uni-id-pages-x/pages/userinfo/bindMobile/bindMobile.test.js
@@ -1,4 +1,3 @@
-// uni-app自动化测试教程: uni-app自动化测试教程: https://uniapp.dcloud.net.cn/worktile/auto/hbuilderx-extension/
 jest.setTimeout(30000)
 const PAGE_PATH = '/uni_modules/uni-id-pages-x/pages/userinfo/bindMobile/bindMobile'
 describe('bindMobile', () => {
@@ -8,9 +7,19 @@ describe('bindMobile', () => {
 		await page.waitFor('view')
 		await page.setData({isTest:true})
 		captcha = "1234"
-		smsCodeEl = await page.$('.smsCodeTest')
-    // console.log('smsCodeEl',smsCodeEl)
+		smsCodeEl = await page.$('#smsCodeTest')
+    console.log('smsCodeEl',smsCodeEl)
 	});
+  // 随机生成手机号
+  function generateRandomPhoneNumber() {
+    const prefixes = ['130', '131', '132', '133', '134', '135', '136', '137', '138', '139', '150', '151', '152', '153', '155', '156', '157', '158', '159', '180', '181', '182', '183', '184', '185', '186', '187', '188', '189'];
+    const prefix = prefixes[Math.floor(Math.random() * prefixes.length)];
+    const areaCode = Math.floor(1000 + Math.random() * 9000).toString();
+    const randomNumber = Math.floor(1000 + Math.random() * 9000).toString();
+    const phoneNumber = prefix + areaCode + randomNumber;
+    return phoneNumber;
+  }
+
   async function setCaptcha(){
     const needCaptcha = await page.data('needCaptcha')
     if(needCaptcha){
@@ -18,6 +27,8 @@ describe('bindMobile', () => {
     }
   }
   async function setSmsCode(mobile){
+    // console.log("data----",await page.data())
+    // console.log("smsCodeEl---data",await smsCodeEl.data())
     await smsCodeEl.setData({
     	mobile:mobile,
     	sendSmsCaptcha:captcha
@@ -27,19 +38,25 @@ describe('bindMobile', () => {
     	smsCode:"123456"
     })
     await setCaptcha()
+    await page.waitFor(2000)
   }
 
   async function getRes(){
     const startTime = Date.now()
+    console.log('startTime',startTime)
+    console.log('testState-----------',await page.data('testState'))
     // 等待登录结果
     await page.waitFor(async () => {
-      if(Date.now()-startTime >10000){
-        console.log('-----------timeout----------')
-        return true
-      }
+      // if(Date.now()-startTime >5000){
+      //   console.log('-----------timeout----------')
+      //   return true
+      // }
     	return await page.data('testState') === true
     })
+    console.log('afterTime',Date.now())
+    console.log('-----------testState',await page.data('testState'))
     const testSuccessRes = await page.data('testSuccess')
+    console.log('testSuccessRes: ',testSuccessRes);
     if(testSuccessRes<100){
       return testSuccess
     }else{
@@ -49,8 +66,10 @@ describe('bindMobile', () => {
   }
 
 	it('绑定手机号', async () => {
-    await setSmsCode("17700000001")
-    // expect.assertions(1);
+    const phone1 = generateRandomPhoneNumber()
+    console.log('phone1: ',phone1);
+    // {mobile: "15566666666", code: "123456", sendSmsCaptcha: "1234"}
+    await setSmsCode(phone1)
     const res = await getRes()
     console.log('res: ',res);
 		if(res<100){
@@ -62,7 +81,7 @@ describe('bindMobile', () => {
 				case 'uni-id-bind-conflict':
 					const expectBindStr = ["此账号已被绑定","This account has been bound"]
 					expect(expectBindStr).toContain(res.errMsg);
-          await setSmsCode("17700000002")
+          await setSmsCode(generateRandomPhoneNumber())
           const res1 = await getRes()
           console.log('res1: ',res1);
 					break;
@@ -74,7 +93,7 @@ describe('bindMobile', () => {
           const requiredStr = ["请输入图形验证码","Captcha required"]
 					expect(requiredStr).toContain(res.errMsg)
           await setCaptcha()
-          await setSmsCode("17700000003")
+          await setSmsCode(generateRandomPhoneNumber())
 					break;
 				default:
           console.log('未知错误')
