af1ee4449b90ccd8857a2587a83a28dc4c431f2c
anne-xml <liuxiaomei@dcloud.io>
2024-12-27 16:23:50 +0800

    test：update userinfo.test.js

--- uni_modules/uni-id-pages-x/pages/userinfo/userinfo.test.js
+++ uni_modules/uni-id-pages-x/pages/userinfo/userinfo.test.js
@@ -1,30 +1,29 @@
-// uni-app自动化测试教程: uni-app自动化测试教程: https://uniapp.dcloud.net.cn/worktile/auto/hbuilderx-extension/
-const PAGE_PATH = '/uni_modules/uni-id-pages-x/pages/userinfo/userinfo'
+// 从index页面跳到userinfo页面，设置showLoginManage=true
+const PAGE_PATH = '/uni_modules/uni-id-pages-x/pages/userinfo/userinfo?showLoginManage=true'
 describe('userinfo', () => {
 	let page,login;
 	beforeAll(async () => {
 		page = await program.navigateTo(PAGE_PATH)
 		await page.waitFor('view')
-		// console.log("pageStack",await program.pageStack())
-	});
-  it('是否登录', async () => {
     login = await page.callMethod('getIsLogin')
     console.log('login',login);
-    if(login){await page.setData({showLoginManage:true})}
-  });
-	it('昵称', async () => {
-    if(!login)return
+    if(!login){
+      console.log('未登录')
+      expect(1).toBe(1)
+      return
+    }
+	});
+	it('昵称-手机号', async () => {
 		const values = await page.$$('.value')
 		await page.waitFor(500)
-		console.log('values:--0 ',await values[0].text());
-		console.log('values:--1',await values[1].text());
-		expect(["dcloud99","dcloud00"]).toContain(await values[0].text())
+		console.log('昵称 ',await values[0].text());
+		console.log('手机号',await values[1].text());
 		expect(await values[1].text()).toHaveLength(11)
 	});
 	it('退出登录', async () => {
 		await page.waitFor(1000)
-    if(!login)return
 		const titleList = await page.$$('.title')
+    console.log('titleList.length',titleList.length)
 		expect(titleList.length).toBe(6)
 		expect(await titleList[5].text()).toBe('退出登录')
 		await page.callMethod('logout')
